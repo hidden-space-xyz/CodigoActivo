61dbb79f166aae10cdc3ef5da66a582fe1303410
Hidden Space <56084434+hidden-space-xyz@users.noreply.github.com>
2026-07-03 15:40:56 +0200

    chore: General code clean

--- backend/CodigoActivo.Domain/Common/Result.cs
+++ backend/CodigoActivo.Domain/Common/Result.cs
@@ -43,7 +43,7 @@ public sealed class Result<T> : Result
     internal Result(Error error)
         : base(false, error)
     {
-        this.value = default;
+        value = default;
     }
 
     public T Value =>
